d2a3076f866788b41dde7eb9ef36a5282095bd53
wssbck <hi@wssbck.cc>
2015-10-17 14:06:54 +0200

    [dip3] chapter 2 - booleans

---
+++ dive-into-python-3/ch02/booleans.py
@@ -0,0 +1,15 @@
+# booleans have two possible values
+t = True
+f = False
+
+# for legacy reasons, in Python 3 they are treated as 1 and 0
+print(t + f)
+print(t - t)
+print(2 * t)
+
+# boolean evaluation
+print(3 < 2)
+print(2 is 2)
+
+# boolean evaluation combined with booleans as numbers
+print((3 > 2) + (2 is 2))
