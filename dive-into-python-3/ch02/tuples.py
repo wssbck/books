0c239cb1f40198e673515628a14d23cf069d26f0
wssbck <hi@wssbck.cc>
2016-02-28 11:21:48 +0100

    [dip3] chapter 2 - tuples

---
+++ dive-into-python-3/ch02/tuples.py
@@ -0,0 +1,41 @@
+# Tuples are immutable but work similarily to lists
+a_tuple = ("a", "b", "mpilgrim", "z", "example")
+print( a_tuple[0]   )
+print( a_tuple[-1]  )
+print( a_tuple[1:3] )
+
+# The following would be an error:
+# a_tuple.append("new")
+
+# Immutability goes pair in pair with being more performant tha lists.
+# It also allows to protect data that is not supposed to be changed.
+
+# In boolean context:
+def is_it_true(anything):
+	if anything:
+		print("yes, it's true")
+	else:
+		print("no, it's false")
+
+is_it_true( a_tuple )
+is_it_true( () )
+is_it_true( (False,) )
+
+# Warning! Parentheses with one element do not constitute a tuple.
+# Python just assumes that it is an extra pair of parentheses.
+print( type((False)) )
+# A coma is needed. This seems silly at first.
+print( type((False,)) )
+
+# Using tuples for multple assignment
+v = ('a', 2, True)
+(x, y, z) = v
+print(x)
+print(y)
+print(z)
+
+# However it will not work if the number of values does not match
+# v = ('a', 2, True, 'd')
+# (x, y, z) = v
+# w = (2,2)
+# (x, y, z) = w
