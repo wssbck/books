0a52dc744bae22b06ea3f0f449400b16bbe64258
wssbck <hi@wssbck.cc>
2016-04-14 23:29:35 +0200

    [dip3] chapter 2 - none

---
+++ dive-into-python-3/02_07_none.py
@@ -0,0 +1,14 @@
+# None is like null, with its own type
+
+a = None
+print(a)
+print(type(a))
+
+# None equals only None
+print(None == None)
+print(None == False)
+
+# There is only one None object created
+a = None
+b = None
+print(a == b)
