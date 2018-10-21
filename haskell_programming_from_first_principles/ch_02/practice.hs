fe85b1d905a99aaec921b86f189e4ba05f134176
wssbck <hi@wssbck.cc>
2018-10-21 22:05:59 +0200

    [hpfp] Chapter 2 code

---
+++ haskell_programming_from_first_principles/ch_02/practice.hs
@@ -0,0 +1,16 @@
+v1        = x * y
+  where x = 5
+        y = 6
+
+v2        = x * 3 + y
+  where x = 3
+        y = 1000
+
+v3        = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+v4        = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
