8eb6002835ec178c910df2735cc9e6eba4b2df98
wssbck <hi@wssbck.cc>
2019-01-05 11:31:45 +0100

    [7l7w] update Scala script

--- seven_languages_in_seven_weeks/scala/day_01/05_tuples.scala
+++ seven_languages_in_seven_weeks/scala/day_01/05_tuples.scala
@@ -1,6 +1,7 @@
-val tuple = ("Something", "tupled")
-val (str1, str2) = tuple
+val tuple = ("Something", "tupled", "try")
+val (str1, str2, str3) = tuple
 
 println(tuple)
 println(str1)
 println(str2)
+println(str3)
